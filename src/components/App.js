126ce8fc06aebd0de55f0d08f502d42e8602ad98
Sina Syrous <sina.syrous@etraveligroup.com>
2020-05-09 21:44:02 +0200

    First commit with dependencies and initial Header component

---
+++ src/components/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+import Header from "./Header.js";
+
+// for authentication using auth0
+import { useAuth0 } from "../auth/react-auth0-wrapper";
+
+// for routing
+import { Switch, Route } from "react-router-dom";
+
+// for apollo client
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { setContext } from "apollo-link-context";
+
+function App() {
+  const { isAuthenticated, user } = useAuth0();
+
+  // used state to get accessToken through getTokenSilently(), the component re-renders when state changes, thus we have
+  // our accessToken in apollo client instance.
+  const [accessToken, setAccessToken] = useState("");
+
+  const { getTokenSilently, loading } = useAuth0();
+  if (loading) {
+    return "Loading...";
+  }
+
+  // get access token
+  const getAccessToken = async () => {
+    // getTokenSilently() returns a promise
+    try {
+      const token = await getTokenSilently();
+      setAccessToken(token);
+      console.log(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  getAccessToken();
+
+  // for apollo client
+  const httpLink = new HttpLink({
+    uri: "https://instagram-clone-3.herokuapp.com/v1/graphql",
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = accessToken;
+    if (token) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${token}`,
+        },
+      };
+    } else {
+      return {
+        headers: {
+          ...headers,
+        },
+      };
+    }
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Header />
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "./auth/react-auth0-wrapper";
+import config from "./auth/auth_config.json";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = (appState) => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      audience={config.audience}
+      redirect_uri={config.redirect_uri}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
